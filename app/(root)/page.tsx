343799e34bb2ac536afd00801339e54150de8668
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-09 22:30:37 +0530

    ui updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,8 @@
-import { UserButton } from "@clerk/nextjs";
  
 export default function Home() {
   return (
     <div>
-      <UserButton afterSignOutUrl="/"/>
+      <h1 className="head-text text-left" >home</h1>
     </div>
   )
 }
---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LeftSidebar = () => {
+  return (
+    <div>
+      LeftSidebar
+    </div>
+  )
+}
+
+export default LeftSidebar
