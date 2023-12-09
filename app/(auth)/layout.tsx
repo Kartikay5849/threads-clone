7b4908ff5e3a2dfc6ee6af5d6717bbd741c602dc
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-09 21:49:50 +0530

    auth using clerk

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,11 @@
 import { ClerkProvider } from "@clerk/nextjs"
-
+import { Inter } from "next/font/google"
+import '../globals.css'
 export const metadata={
     title:'Threads',
     description:'Nextjs Threads app'
 }
-
+const inter =Inter({subsets:["latin"]})
 export  default function RootLayout({
     children
 }:{
@@ -12,7 +13,11 @@ export  default function RootLayout({
 }) {
     return(
         <ClerkProvider>
-            
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1` }>
+                        {children}
+                </body>
+            </html>
         </ClerkProvider>
     )
 }
