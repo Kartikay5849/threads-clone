106794d10a59599f4f6c82665dc2b013b05481fa
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-09 23:44:03 +0530

    layout done

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,11 +1,44 @@
-import React from 'react'
+"use client"
 
+import React from 'react'
+import { sidebarLinks } from '@/constants';
+import { usePathname,useRouter } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
 const Bottombar = () => {
+  const pathname=usePathname();
   return (
-    <div>
-      Bottombar
-    </div>
-  )
+    <section className='bottombar' >
+      <div className='bottombar_container' >
+      {sidebarLinks.map((link) => {
+          const isActive =
+            (pathname.includes(link.route) && link.route.length > 1) ||
+            pathname === link.route;
+
+          return (
+            <Link
+              href={link.route}
+              key={link.label}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
+            >
+              <Image
+                src={link.imgURL}
+                alt={link.label}
+                width={16}
+                height={16}
+                className='object-contain'
+              />
+
+              <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                {link.label.split(/\s+/)[0]}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+
+    </section>
+  );
 }
 
 export default Bottombar
--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const RightSidebar = () => {
   return (
-    <div>
-      RightSidebar
-    </div>
+    <section className='custom-scrollbar rightsidebar' >
+      <div className='flex flex-col flex-1 justify-start'>
+        <h3 className='text-heading4-medium text-light-1' >
+          Suggested Communities
+        </h3>
+      </div>
+      <div className='flex flex-col flex-1 justify-start'>
+        <h3 className='text-heading4-medium text-light-1' >
+          Suggested Users
+        </h3>
+      </div>
+
+    </section>
   )
 }
 
