c36454fdc484a96df646c0ff5c0be4ebe1947696
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-17 23:16:33 +0530

    create thread ui and backend

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -1,6 +1,18 @@
 import React from "react"
+import { currentUser } from "@clerk/nextjs"
+import {redirect} from "next/navigation"
+import { fetchUser } from "@/lib/actions/user.actions";
+import PostThread from "@/components/forms/PostThread";
 async function Page() {
-    return <h1 className="head-text" >Create Threads</h1>
+    const user=await currentUser();
+    if(!user) return null;
+    const userInfo=await fetchUser(user.id);
+    if(!userInfo?.onboarded) redirect("/onboarding");
+    return<>
+    
+         <h1 className="head-text" >Create Threads</h1>
+        <PostThread userId={userInfo._id}/>
+    </>
     
 }
 export default Page
---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,6 @@
+ function PostThread({userId} :{userId:string}){
+    return (
+        <h1 className="head-text" >Post Thread form</h1>
+    )
+}
+export default PostThread;
