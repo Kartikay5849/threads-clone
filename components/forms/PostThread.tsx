05822c55954764104d986a4bee7badd630f768ca
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-19 11:12:43 +0530

    thread structural updates

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -16,45 +16,56 @@ import {
 } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { ThreadValidation } from "@/lib/validations/thread";
 
-// import { ThreadValidation } from "@/lib/validations/thread";
-// import { createThread } from "@/lib/actions/thread.actions";
- function PostThread({userId} :{userId:string}){
-    const form = useForm<z.infer<typeof ThreadValidation>>({
-        resolver: zodResolver(ThreadValidation),
-        defaultValues: {
-          thread: "",
-          accountId: userId,
-        },
-      });
+interface Props {
+  userId: string;
+}
+
+  
+
+function PostThread({userId}:{userId:string}){
+    const router = useRouter();
+  const pathname = usePathname();
+ 
+
+  const form = useForm<z.infer<typeof ThreadValidation>>({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+        thread:'',
+        accountId:userId
+    },
+  });
+  const onSubmit=()=>{
+    
+  }
     return (
         <Form {...form}>
-        <form
-          className='mt-10 flex flex-col justify-start gap-10'
-          onSubmit={form.handleSubmit(onSubmit)}
-        >
-          <FormField
-            control={form.control}
-            name='thread'
-            render={({ field }) => (
-              <FormItem className='flex w-full flex-col gap-3'>
-                <FormLabel className='text-base-semibold text-light-2'>
-                  Content
-                </FormLabel>
-                <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
-                  <Textarea rows={15} {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-  
-          <Button type='submit' className='bg-primary-500'>
-            Post Thread
-          </Button>
-        </form>
-      </Form>
-    );
-  }
-  
-export default PostThread;
+      <form
+        className='mt-10 flex flex-col justify-start gap-10'
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
+          control={form.control}
+          name='thread'
+          render={({ field }) => (
+            <FormItem className='flex w-full flex-col gap-3'>
+              <FormLabel className='text-base-semibold text-light-2'>
+                Content
+              </FormLabel>
+              <FormControl className='no-focus border border-dark-4 bg-dark-3 text-light-1'>
+                <Textarea rows={15} {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type='submit' className='bg-primary-500'>
+          Post Thread
+        </Button>
+      </form>
+    </Form>
+  );
+}
+export default PostThread
