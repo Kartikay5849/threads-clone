6740fe09956a5eafad28d607286b97e5d25c1f0c
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-20 11:04:33 +0530

    card ui update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,22 +6,26 @@ export  default async function Home() {
  const user= await currentUser();
   return (
     <div>
-      <h1 className="head-text text-left" >home</h1>
-      <section className="mt-9 flex flex-col gap-10" >
-        {result.posts.length ===0?(<p className="no-result" >No Threads Found</p>):(
+       
+      <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
           <>
-            {result.posts.map((post)=>(
+            {result.posts.map((post) => (
               <ThreadCard
-              key={post._id}
-              id={post._id}
-              currentUserId={user?.id}
-              parentId={post.parentId}
-              content={post.text}
-              author={post.author}
-              community={post.community}
-              createdAt={post.createdAt}
-              comments={post.children}
-            />
+                key={post._id}
+                id={post._id}
+                currentUserId={user?.id ||''}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
             ))}
           </>
         )}
