fed2e30ab46f429aca3496996dada369d75e20e3
Kartikay5849 <kartikaysambher@gmail.com>
2023-12-22 10:43:26 +0530

    updates

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -3,6 +3,9 @@ import { currentUser } from "@clerk/nextjs"
 import {redirect} from "next/navigation"
 import { fetchUser } from "@/lib/actions/user.actions";
 import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import Image from "next/image";
+import { profileTabs } from "@/constants";
 async function Page({params}:{params:{id:string}}) {
     const user=await currentUser();
     if(!user) return null;
@@ -19,6 +22,32 @@ async function Page({params}:{params:{id:string}}) {
               bio={userInfo.bio}
 
             />
+            <div className="mt-9">
+            <Tabs defaultValue='threads' className='w-full'>
+             <TabsList className='tab'>
+              {profileTabs.map((tab) => (
+                <TabsTrigger key={tab.label} value={tab.value} className='tab'>
+                    <Image
+                        src={tab.icon}
+                        alt={tab.label}
+                        width={24}
+                        height={24}
+                        className='object-contain'
+                />
+                 <p className='max-sm:hidden'>{tab.label}</p>
+                         {tab.label === "Threads" && (
+                        <p className='ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2'>
+                            {userInfo.threads.length}
+                        </p>
+                )}
+                </TabsTrigger>
+
+                            ))}
+                        
+                    </TabsList>
+                </Tabs>
+
+            </div>
         </div>
     )
 }
